9f633544ee393c2b99dcc6e35c5b0371d80978c2
Ahsan Ullah <ahsanullahsunsbd@gmail.com>
2021-06-01 21:30:58 +0600

    change

--- ProblemSolving.js
+++ ProblemSolving.js
@@ -1,6 +1,29 @@
-let Input = "Bangladesh";
+let Input = "BanglaDesh";
 
-function reverseString(string) {
-	return string.split("").reverse().join("");
+function reverseString(str) {
+	var findUpperCase = [];
+	for (let index = 0; index < str.length; index++) {
+		if (str[index] >= "A" && str[index] <= "Z") {
+			findUpperCase.push(index);
+		}
+	}
+	var makeArray = str.split("").reverse();
+	var create = [];
+	for (let i = 0; i < makeArray.length; i++) {
+		for (let j = 0; j < findUpperCase.length; j++) {
+			if (i !== findUpperCase[j]) {
+				create.push(makeArray[i].toLowerCase());
+				break;
+			} else {
+				console.log(findUpperCase[j]);
+				const upperCaseLetter = makeArray[i].toUpperCase();
+				create.push(upperCaseLetter);
+				break;
+			}
+		}
+	}
+
+	return create.join("");
 }
-console.log(reverseString(Input));
+const output = reverseString(Input);
+console.log(output);
